5c092ca3e89f2efd1e9ae75ceb00f32fa5ce4761
santiago <velazquezespinola@gmail.com>
2017-04-06 18:17:09 -0300

    test fallido para ver si la ic funciona

---
+++ pp2/src/test/java/maven/FailTest.java
@@ -0,0 +1,15 @@
+package maven;
+
+import junit.framework.TestCase;
+
+public class FailTest extends TestCase {
+
+	public FailTest (String name) {
+		super(name);
+	}
+	
+	public void test() {
+		assertTrue( false );
+	}
+	
+}
